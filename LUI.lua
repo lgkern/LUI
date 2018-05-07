05222b6c7244139365fd7b9e4582027277ba63c0
Gavin Grieve <hektik@dimebox.net>
2018-05-07 18:43:47 +1200

    Update TOC and LIVE_BUILD number

--- LUI.lua
+++ LUI.lua
@@ -26,7 +26,7 @@ LUI.Versions = {lui = 3403}
 LUI.dummy = function() return end
 
 local LIVE_TOC = 70300
-local LIVE_BUILD = 25549
+local LIVE_BUILD = 26365
 -- Check the build to compare with PTR
 local _, CURRENT_BUILD, _, CURRENT_TOC = GetBuildInfo()
 if tonumber(CURRENT_BUILD) > LIVE_BUILD then
